d367431cd749b65567785822f55e8a951d0a229f
rrrl12 <ryotasekizuka@hiroshima-u.ac.jp>
2019-06-17 15:26:42 +0900

    add processing

---
+++ src/main/kotlin/rrrl12/spaceMouseManager/Main.kt
@@ -0,0 +1,24 @@
+package rrrl12.spaceMouseManager
+
+import processing.core.*
+
+
+fun main(args: Array<String>) = PApplet.main("rrrl12.spaceMouseManager.Main")
+
+
+class Main : PApplet() {
+
+    override fun settings() {
+        size(200, 200)
+    }
+
+
+    override fun setup() {
+
+    }
+
+
+    override fun draw() {
+
+    }
+}
