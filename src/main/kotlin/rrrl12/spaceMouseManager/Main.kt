51616a72e2e5236f4d7aa5c8409805b6088f4004
rrrl12 <ryotasekizuka@hiroshima-u.ac.jp>
2019-06-18 13:49:41 +0900

    spaceMouse

--- src/main/kotlin/rrrl12/spaceMouseManager/Main.kt
+++ src/main/kotlin/rrrl12/spaceMouseManager/Main.kt
@@ -2,9 +2,7 @@ package rrrl12.spaceMouseManager
 
 
 import processing.core.*
-import org.gamecontrolplus.ControlDevice
 import org.gamecontrolplus.ControlIO
-import org.gamecontrolplus.ControlSlider
 
 
 
@@ -16,29 +14,33 @@ class Main : PApplet() {
     private var spaceMouseL: SpaceMouse? = null
     private var spaceMouseR: SpaceMouse? = null
 
-    lateinit var factory: SpaceMouseFactory
+    private lateinit var factory: SpaceMouseFactory
 
-    var device: ControlDevice? = null
-    var slider: ControlSlider? = null
+    private lateinit var oscManager: OscManager
+
     override fun settings() {
         size(200, 200)
     }
 
 
     override fun setup() {
-        val control = ControlIO.getInstance(this)
-        val deviceList = control.devices.toList()
-        println(deviceList)
-
         factory = SpaceMouseFactory(this)
         spaceMouseL = factory.create("SpaceMouse Wireless")
         spaceMouseR = factory.create("SpaceMouse Wireless")
         println(factory.usedDeviceIdList)
 
+        oscManager = OscManager(this, "127.0.0.1", 12000, 11999)
     }
 
 
     override fun draw() {
+        val mouseStateL = spaceMouseL?.getState()
+        val mouseStateR = spaceMouseR?.getState()
+
         println(spaceMouseL?.getState())
+
+
+        oscManager.sendSpaceMouseState("/mouse", mouseStateL!!, mouseStateR!!)
+
     }
 }
--- src/main/kotlin/rrrl12/spaceMouseManager/OscManager.kt
+++ src/main/kotlin/rrrl12/spaceMouseManager/OscManager.kt
@@ -1,4 +1,44 @@
 package rrrl12.spaceMouseManager
 
-class OscManager {
+import netP5.NetAddress
+import oscP5.OscMessage
+import oscP5.OscP5
+import processing.core.PApplet
+
+class OscManager(app: PApplet, outAddress: String, outPort: Int, inPort: Int) {
+
+    private val osc = OscP5(app, inPort)
+    private val remoteRocation = NetAddress(outAddress, outPort)
+
+
+    fun sendFloatList(patterns: String, args: List<Float>) {
+        val message = OscMessage(patterns)
+        args.forEach { arg -> message.add(arg) }
+        osc.send(message, remoteRocation)
+    }
+
+
+    fun sendSpaceMouseState(patterns: String, stateL: SpaceMouseState, stateR: SpaceMouseState) {
+        val message = OscMessage(patterns).apply {
+            stateL.let {
+                add(it.rx)
+                add(it.ry)
+                add(it.rz)
+                add(it.x)
+                add(it.y)
+                add(it.z)
+            }
+            stateR.let {
+                add(it.rx)
+                add(it.ry)
+                add(it.rz)
+                add(it.x)
+                add(it.y)
+                add(it.z)
+            }
+        }
+
+        osc.send(message, remoteRocation)
+    }
+
 }
